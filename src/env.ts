1e084d6df6014f15cfb856f4e8df84a6aa539739
Dmytro Melnyk <dmelnyk@darkstoreapp.com>
2023-07-20 16:24:18 +0000

    refactoring

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,5 @@
 export interface Env {
   ATLAS_APP_KEY: string;
   ATLAS_APP_ID: string;
-  FAUNA_KEY: string;
-  FAUNA_DOMAIN: string;
-  FAUNA_SCHEME: "http" | "https";
-  FAUNA_PORT: number;
   WORKER_DATA: KVNamespace;
 }
